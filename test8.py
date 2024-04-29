5100c38ff3bd0ca2fe9ac0b04734547a1389d95e
shweyaminmyat <shweyaminmyat0@gmail.com>
2024-04-29 10:32:56 +0700

    test9

--- test8.py
+++ test8.py
@@ -1,9 +1,16 @@
 myfav_artists = ['Ed Shareen', 'Abba', 'Htoo Eain Tin','Taylor Swift']
-
-print(type(myfav_artists))
+myfav_artists[0] = 'The one who sang my fav song Afterglow'
 print(myfav_artists)
-print(myfav_artists[2])
-print(len(myfav_artists))
-print(myfav_artists[0:3])
-print(myfav_artists[1:])
-print(myfav_artists[:4])
+# myfav_artists.sort()
+# print(myfav_artists)
+# print(myfav_artists)
+# myfav_artists.insert(2, 'Shocking Blues')
+# print(myfav_artists)
+
+# print(type(myfav_artists))
+# print(myfav_artists)
+# print(myfav_artists[2])
+# print(len(myfav_artists))
+# print(myfav_artists[0:3])
+# print(myfav_artists[1:])
+# print(myfav_artists[:4])
---
+++ test9.py
@@ -0,0 +1,9 @@
+artist_TayTay = {
+    'name' : 'Taylor Swift',
+    'boyfriend' : 'Travis Kelce'
+}
+
+print(type(artist_TayTay))
+print(artist_TayTay['name'])
+print(artist_TayTay['boyfriend'])
+artist_TayTay["Network"] = "1.1billion dollars"
