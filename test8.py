57f2187b64326d6f3ddb00354f314fbf9a493a95
shweyaminmyat <shweyaminmyat0@gmail.com>
2024-04-29 09:53:45 +0700

    test8

--- test8.py
+++ test8.py
@@ -1,1 +1,9 @@
-print("ta ku ku")
+myfav_artists = ['Ed Shareen', 'Abba', 'Htoo Eain Tin','Taylor Swift']
+
+print(type(myfav_artists))
+print(myfav_artists)
+print(myfav_artists[2])
+print(len(myfav_artists))
+print(myfav_artists[0:3])
+print(myfav_artists[1:])
+print(myfav_artists[:4])
